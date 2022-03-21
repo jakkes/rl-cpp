bfce8f4a0017b629f08e42d3789c3569184dff0e
Jakob Stigenberg <jakob@jakke.se>
2022-03-21 22:22:50 +0100

    base handler

---
+++ include/rl/logging/handler/base.h
@@ -0,0 +1,13 @@
+#ifndef RL_LOGGING_HANDLER_BASE_H_
+#define RL_LOGGING_HANDLER_BASE_H_
+
+
+namespace rl::logging::handler
+{
+    class Base
+    {
+        
+    };
+}
+
+#endif /* RL_LOGGING_HANDLER_BASE_H_ */

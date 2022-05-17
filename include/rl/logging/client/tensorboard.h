52b9b863051076f962d7e4497dc5554717c0cddb
Jakob Stigenberg <jakob@jakke.se>
2022-05-17 19:22:11 +0200

    add tensorboard logger

---
+++ include/rl/logging/client/tensorboard.h
@@ -0,0 +1,46 @@
+#ifndef RL_LOGGING_CLIENT_TENSORBOARD_H_
+#define RL_LOGGING_CLIENT_TENSORBOARD_H_
+
+
+#include <mutex>
+#include <unordered_map>
+#include <string>
+
+#include <tensorboard_logger.h>
+
+#include "base.h"
+#include "rl/option.h"
+#include "rl/cpputils/metronome.h"
+
+
+namespace rl::logging::client
+{
+    struct TensorboardOptions
+    {
+        RL_OPTION(size_t, frequency_window) = 5;
+    };
+
+    class Tensorboard : public Base
+    {
+        public:
+            Tensorboard(const TensorboardOptions &options={});
+
+            void log_scalar(const std::string &name, double value) override;
+            void log_frequency(const std::string &name, int occurances) override;
+            void log_text(const std::string &name, const std::string &value) override;
+
+        private:
+            const TensorboardOptions options;
+
+            TensorBoardLogger logger;
+            std::recursive_mutex mtx{};
+            std::unordered_map<std::string, size_t> steps{};
+            std::unordered_map<std::string, size_t> occurances{};
+            
+            rl::cpputils::Metronome<std::chrono::seconds> metronome;
+
+            void metronome_callback();
+    };
+}
+
+#endif /* RL_LOGGING_CLIENT_TENSORBOARD_H_ */
---
+++ src/rl/logging/client/tensorboard.cc
@@ -0,0 +1,50 @@
+#include "rl/logging/client/tensorboard.h"
+
+#include <filesystem>
+
+
+namespace rl::logging::client
+{
+    static
+    std::string get_name() {
+        if (!std::filesystem::exists("runs")) {
+            std::filesystem::create_directory("runs");
+        }
+
+        size_t count{0};
+        for (const auto &entry : std::filesystem::directory_iterator("runs")) {
+            count++;
+        }
+
+        return "runs/run_" + std::to_string(count) + ".tfevents";
+    }
+
+    Tensorboard::Tensorboard(const TensorboardOptions &options)
+    : logger{get_name(), false}, options{options}, metronome{options.frequency_window}
+    {
+        metronome.start(std::bind(&Tensorboard::metronome_callback, this));
+    }
+
+    void Tensorboard::log_scalar(const std::string &name, double value) {
+        std::lock_guard lock{mtx};
+        logger.add_scalar(name, steps[name]++, value);
+    }
+
+    void Tensorboard::log_text(const std::string &name, const std::string &value) {
+        std::lock_guard lock{mtx};
+        logger.add_text(name, steps[name]++, value.c_str());
+    }
+
+    void Tensorboard::log_frequency(const std::string &name, int occurances) {
+        std::lock_guard lock{mtx};
+        this->occurances[name] += occurances;
+    }
+
+    void Tensorboard::metronome_callback() {
+        std::lock_guard lock{mtx};
+        for (const auto &item : occurances) {
+            log_scalar(item.first, item.second * 1.0 / options.frequency_window);
+            occurances[item.first] = 0;
+        }
+    }
+}

1fd72a6eb4a473c5db1cd2f4622478042cecee8b
Jakob Stigenberg <jakob@jakke.se>
2022-07-09 22:24:59 +0200

    fix args

--- examples/seedppo_continuous_cartpole.cc
+++ examples/seedppo_continuous_cartpole.cc
@@ -8,41 +8,90 @@ using namespace rl;
 
 argparse::ArgumentParser parse_args(int argc, char **argv)
 {
-    argparse::ArgumentParser parser{};
-    parser
-        .add_argument("--duration")
-        .help("Train duration, seconds.")
-        .default_value(3600)
-        .scan<'i', int>();
+    argparse::ArgumentParser args{};
     
-    parser
-        .add_argument("--envs-per-worker")
-        .help("Number of environment sequences handled in parallell.")
-        .default_value<int>(8)
-        .scan<'i', int>();
-
-    parser
-        .add_argument("--env-workers")
-        .help("Environment rollouts are parallellized across threads.")
-        .default_value<int>(16)
-        .scan<'i', int>();
-
-    parser
+    args
         .add_argument("--cuda")
-        .help("If set, training is run on the GPU.")
-        .default_value<bool>(false)
+        .help("If set, training happens on GPU.")
+        .default_value<bool>(true)
         .implicit_value(true);
 
+    args
+        .add_argument("--batch-size")
+        .help("Batch size used in training.")
+        .default_value<int>(64)
+        .scan<'i', int>();
+
+    args
+        .add_argument("--env-workers")
+        .help("Number of threads executing environment steps.")
+        .default_value<int>(4)
+        .scan<'i', int>();
+    
+    args
+        .add_argument("--envs-per-worker")
+        .help("Number of environments executed per environment worker.")
+        .default_value<int>(4)
+        .scan<'i', int>();
+    
+    args
+        .add_argument("--inference-batch-size")
+        .help("Maximum batch size per inference request.")
+        .default_value<int>(16)
+        .scan<'i', int>();
+    
+    args
+        .add_argument("--inference-delay")
+        .help("Maximum delay (milliseconds) before inference requests are processed.")
+        .default_value<int>(100)
+        .scan<'i', int>();
+
+    args
+        .add_argument("--inference-replay-size")
+        .help("Number of sequences collected before added (batched) into the training replay.")
+        .default_value<int>(500)
+        .scan<'i', int>();
+    
+    args
+        .add_argument("--min-replay-size")
+        .help("Size of replay when training may start.")
+        .default_value<int>(1000)
+        .scan<'i', int>();
+
+    args
+        .add_argument("--replay-size")
+        .help("Size of training replay buffer.")
+        .default_value<int>(1000)
+        .scan<'i', int>();
+    
+    args
+        .add_argument("--sequence-length")
+        .help("Roll out sequence lengths.")
+        .default_value<int>(64)
+        .scan<'i', int>();
+    
+    args
+        .add_argument("--max-update-frequency")
+        .help("Maximum number of training steps executed per second.")
+        .default_value<float>(25.0f)
+        .scan<'g', float>();
+    
+    args
+        .add_argument("--duration")
+        .help("Training duration (seconds).")
+        .default_value<int>(3600)
+        .scan<'i', int>();
+
     try {
-        parser.parse_args(argc, argv);
+        args.parse_args(argc, argv);
     }
     catch (const std::runtime_error& err) {
         std::cerr << err.what() << std::endl;
-        std::cerr << parser;
+        std::cerr << args;
         std::exit(1);
     }
 
-    return parser;
+    return args;
 }
 
 class Model : public rl::agents::ppo::Module
@@ -88,9 +137,8 @@ int main(int argc, char **argv)
     auto logger = std::make_shared<logging::client::Tensorboard>();
     auto env_factory = std::make_shared<env::CartPoleContinuousFactory>(200);
     env_factory->set_logger(logger);
-    
+
     if (args.get<bool>("--cuda")) {
-        env_factory->cuda();
         model->to(torch::kCUDA);
     }
     auto optimizer = std::make_shared<torch::optim::Adam>(model->parameters());
@@ -100,20 +148,20 @@ int main(int argc, char **argv)
         optimizer,
         env_factory,
         agents::ppo::trainers::SEEDOptions{}
-            .batchsize_(64)
+            .batchsize_(args.get<int>("--batch-size"))
             .env_workers_(args.get<int>("--env-workers"))
             .envs_per_worker_(args.get<int>("--envs-per-worker"))
-            .inference_batchsize_(args.get<int>("--env-workers") * args.get<int>("--envs-per-worker") / 2)
-            .inference_max_delay_ms_(500)
-            .inference_replay_size_(100)
-            .min_replay_size_(1000)
-            .replay_size_(1000)
+            .inference_batchsize_(args.get<int>("--inference-batch-size"))
+            .inference_max_delay_ms_(args.get<int>("--inference-delay"))
+            .inference_replay_size_(args.get<int>("--inference-replay-size"))
+            .min_replay_size_(args.get<int>("--min-replay-size"))
+            .replay_size_(args.get<int>("--replay-size"))
+            .sequence_length_(args.get<int>("--sequence-length"))
+            .max_update_frequency_(args.get<float>("--max-update-frequency"))
+            .logger_(logger)
             .replay_device_(args.get<bool>("--cuda") ? torch::kCUDA : torch::kCPU)
             .network_device_(args.get<bool>("--cuda") ? torch::kCUDA : torch::kCPU)
             .environment_device_(torch::kCPU)
-            .sequence_length_(64)
-            .max_update_frequency_(25)
-            .logger_(logger)
     };
 
     trainer.run(std::chrono::seconds(args.get<int>("--duration")));

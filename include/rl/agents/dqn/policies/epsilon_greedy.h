910797dee2fe928a2b4552fcf86d181b6792219b
Jakob Stigenberg <jakob@jakke.se>
2023-04-30 15:27:48 +0200

    epsilon greedy with float control

--- include/rl/agents/dqn/policies/epsilon_greedy.h
+++ include/rl/agents/dqn/policies/epsilon_greedy.h
@@ -3,6 +3,8 @@
 
 
 #include "base.h"
+#include <rl/utils/float_control/base.h>
+#include <rl/utils/float_control/fixed.h>
 
 namespace rl::agents::dqn::policies
 {
@@ -10,18 +12,20 @@ namespace rl::agents::dqn::policies
     {
         public:
 
-            EpsilonGreedy(float epsilon) : epsilon{epsilon} {}
+            EpsilonGreedy(std::shared_ptr<rl::utils::float_control::Base> epsilon)
+            : epsilon{epsilon} {}
+
+            EpsilonGreedy(float epsilon)
+            : EpsilonGreedy(std::make_shared<rl::utils::float_control::Fixed>(epsilon))
+            {}
 
             ~EpsilonGreedy() = default;
 
             std::unique_ptr<rl::policies::Categorical> policy(
                     const rl::agents::dqn::modules::BaseOutput &output) override;
 
-            inline
-            void update_epsilon(float epsilon) { this->epsilon = epsilon; }
-
         private:
-            float epsilon;
+            std::shared_ptr<rl::utils::float_control::Base> epsilon;
     };
 }
 
